052734ee26219aee366f7bacc4dadfc9de14a58b
Denys Maksymchuk <denysmaksimchuk@gmail.com>
2023-02-08 16:00:33 +0100

    Fixed

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,7 @@
 const inputEl = document.querySelector("#name-input");
 const spanEl = document.querySelector("#name-output");
 inputEl.addEventListener("input", (event) => {
-  if (inputEl.value === "") {
+  if (inputEl.value.trim() === "") {
     return (spanEl.textContent = "Anonymous");
   }
   spanEl.textContent = event.currentTarget.value;
--- js/task-08.js
+++ js/task-08.js
@@ -1,13 +1,16 @@
 const form = document.querySelector(".login-form");
 const loginFormSubmitHeandler = (event) => {
   event.preventDefault();
-  const email = event.currentTarget.elements.email.value;
+  const email = event.currentTarget.email.value.trim();
   const password = event.currentTarget.elements.password.value;
   const formData = {
     email,
     password,
   };
-  email === "" || password === "" ? alert("Заповніть форму") : console.log(formData);
+  if (email === "" || password === "") {
+    alert("Заповніть форму");
+  } else console.log(formData);
+
   event.currentTarget.reset();
 };
 form.addEventListener("submit", loginFormSubmitHeandler);
