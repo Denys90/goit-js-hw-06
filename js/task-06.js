4cf0594a9110f0392de18b91a3d66ca9ce5194f9
Denys Maksymchuk <denysmaksimchuk@gmail.com>
2023-02-08 21:02:00 +0100

    Вибачаюсь)

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,8 @@
 const textInput = document.querySelector("#validation-input");
 
 textInput.addEventListener("blur", (event) => {
-  if (textInput.velue.trim().length === Number(textInput.dataset.length)) {
-    textInput.classList.remove("invalid");
+  textInput.classList.remove("invalid", "valid");
+  if (textInput.value.trim().length === Number(textInput.dataset.length)) {
     return textInput.classList.add("valid");
   }
   return textInput.classList.add("invalid");
